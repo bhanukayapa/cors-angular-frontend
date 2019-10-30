252f249c6485a4c2d874c069ed76f39e3e4523e4
bhanukayapa <bhanukayapa@gmail.com>
2019-10-30 16:10:07 +0530

    localizing

--- src/app/services/rest.service.ts
+++ src/app/services/rest.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class RestService {
 
-  private REST_API_SERVER = 'https://cors-backend-service.herokuapp.com/api/';
+  private REST_API_SERVER = 'http://client2.local.com:4200/api/';
 
   constructor(private httpClient: HttpClient) { }
 
